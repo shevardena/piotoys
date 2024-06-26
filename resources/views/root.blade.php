8e7b9585f04b23c1afaca70c9054d6a79fd7116f
Irakli Shevardenidze <iraklig2@gmail.com>
2024-06-26 18:54:42 +0400

    Removed bladewind

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -3,13 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
-    <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
     @spladeHead
-    <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
     @vite('resources/js/app.js')
 </head>
 <body class="antialiased">
--- storage/framework/views/1c4c42e47145b6ecdf5387bc8234e7a8.php
+++ storage/framework/views/1c4c42e47145b6ecdf5387bc8234e7a8.php
@@ -3,14 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
-    <link href="<?php echo e(asset('vendor/bladewind/css/animate.min.css')); ?>" rel="stylesheet" />
-    <link href="<?php echo e(asset('vendor/bladewind/css/bladewind-ui.min.css')); ?>" rel="stylesheet" />
     <?php echo e(app('laravel-splade-seo')->renderHead()); ?>
 
-    <script src="<?php echo e(asset('vendor/bladewind/js/helpers.js')); ?>"></script>
     <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
 </head>
 <body class="antialiased">
